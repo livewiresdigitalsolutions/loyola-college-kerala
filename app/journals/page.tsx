0f34dac3bd69dc30972091279b5abd635f1f96a2
your.user.name <112639423+Manavalan2517@users.noreply.github.com>
2026-02-14 09:43:39 +0530

    Add Archives and JournalCard components

--- app/journals/page.tsx
+++ app/journals/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "./_components/home/Hero";
+import Archives from "./_components/home/Archives";
 
 export default function page() {
   return (
     <main className="min-h-screen bg-background">
       <Hero />
+      <Archives />
     </main>
   );
 }
